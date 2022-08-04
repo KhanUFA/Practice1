8e05dc518d3630e4717d6b68fb0c15a332e55dd4
Roman <lymon700@gmail.com>
2022-08-05 00:49:54 +0500

    changed some tests. Now this test is more important

--- Tests/Tests.java
+++ Tests/Tests.java
@@ -1,16 +1,19 @@
 import org.junit.jupiter.api.*;
 import some.Item;
 import some.Storable;
-import some.containers.Box;
+import some.containers.Bag;
 import some.simple.items.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests{
     @Test
-    void createTest(){
-        Item ball = new Ball("Мяч", 5, 4, "green");
-        assertEquals("Мяч", ball.getName());
-        System.out.println(ball);
+    void exceptionsTest(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            Item ball = new Ball("Мяч", 5, 8, "green");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Item ball = new Ball(null, 5, 0, "green");
+        });
     }
 
     @Test
@@ -21,17 +24,17 @@ public class Tests{
     }
 
     @Test
-    void addToContainer(){
-        Item ball = new Ball("Мяч", 5, 4, "green");
-        Item bigBall = new Ball("Мячище", 5, 10, "green");
+    void checkContainerWeight(){
+        Item ball = new Ball("Мяч", 5.5, 4, "green");
+        Item bigBall = new Ball("Мячище", 5, 4, "green");
         Item MyBall = new Ball("Мячик", 5, 1, "green");
-        Storable box = new Box("Яндекс.Доставка", 0.5, 40);
+        Storable bag = new Bag("Яндекс.Доставка", 0.5, 40);
 
-        box.add(ball);
-        box.add(bigBall);
-        box.add(MyBall);
-        assertNotNull(box.get());
+        bag.add(ball);
+        bag.add(bigBall);
+        bag.add(MyBall);
 
-        System.out.println(box.get().getName());
+        assertEquals(16., bag.getWeight());
+        System.out.println(bag.get().getName());
     }
 }
