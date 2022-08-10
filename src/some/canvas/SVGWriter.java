ed5a28e35463be7865a3f44385c041dec590a5ba
R. Starikov <lymon700@gmail.com>
2022-08-10 12:13:14 +0500

    Simple methods for "drawing" primitives in svg

--- src/some/canvas/SVGWriter.java
+++ src/some/canvas/SVGWriter.java
@@ -3,6 +3,7 @@ package some.canvas;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 public class SVGWriter {
     private File file;
@@ -15,18 +16,39 @@ public class SVGWriter {
                 file.createNewFile();
             }
 
-            fileWriter = new FileWriter();
+            fileWriter = new FileWriter(file);
         } catch (IOException e) {
             System.err.println("Failed to open the file :c");
             e.printStackTrace();
         }
     }
 
-    void fillHead() throws IOException{
-        fileWriter.write("<?xml version=\"1.0\"?>\n<svg width=\"800\" height=\"800\"\nxmlns=\"http://www.w3.org/2000/svg\">");
+    public void fillHead() throws IOException {
+        fileWriter.write("<?xml version=\"1.0\"?>\n<svg width=\"800\" height=\"800\"\nxmlns=\"http://www.w3.org/2000/svg\">\n");
     }
 
-    void drawRect(int x, int y, int width, int height, String colorFill, String colorBorder){
-        String.format("<rect x=\"%d\" y=\"%d\" width=\"100\" height=\"50\" style=\"fill:yellow;stroke:red\"/>");
+    public void drawRect(int x, int y, int width, int height, String colorFill, String colorBorder) throws IOException {
+        String query = String.format("<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:%s;stroke:%s\"/>\n",
+                                        x, y, width, height, colorFill, colorBorder);
+        fileWriter.write(query);
+    }
+
+    public void drawRoundRect(int x, int y, int width, int height, String colorFill, String colorBorder) throws IOException {
+        String query = String.format("<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" rx=\"5\" ry=\"25\" style=\"fill:%s;stroke:%s\"/>\n",
+                                        x, y, width, height, colorFill, colorBorder);
+        fileWriter.write(query);
+    }
+
+    public void drawEllipse(int cx, int cy, int rx, int ry, String colorFill, String colorBorder) throws IOException{
+        String query = String.format("<ellipse cx=\"%d\" cy=\"%d\" rx=\"%d\" ry=\"%d\" style=\"fill:%s;stroke:%s\" stroke-width=\"1\"/>\n",
+                                        cx, cy, rx, ry, colorFill, colorBorder);
+        fileWriter.write(query);
+    }
+
+    public void fillEndOfFile() throws IOException{
+        fileWriter.write("</svg>");
+
+        fileWriter.flush();
+        fileWriter.close();
     }
 }
