1298f02a86d4ea5c356e769e808b296cc81df9e9
Roman <lymon700@gmail.com>
2022-08-11 01:49:24 +0500

    Changed test to test of draw nested containers with items

--- Tests/CanvasTests.java
+++ Tests/CanvasTests.java
@@ -1,21 +1,11 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import some.Item;
-import some.Shape;
 import some.canvas.SVGWriter;
 import some.containers.Bag;
 import some.containers.Box;
 import some.simple.items.Ball;
 import some.simple.items.Brick;
 import some.simple.items.Magazine;
-
-import java.awt.*;
-import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Formatter;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -25,29 +15,32 @@ public class CanvasTests {
         String fileName = "simpleTest";
         SVGWriter svg = new SVGWriter(fileName);
         Item ball = new Ball("Мяч", 0.5, 5, "green");
-        Item brick = new Brick("Кирпич", 0.5, 5, "brown");
+        Item brick = new Brick("Кирпич", 2, 5, "brown");
         Item magazine = new Magazine("Журнал", 1, 5, "grey");
-        Box box = new Box("Яндекс.Доставка", 0.5, 10);
+        Item ball1 = new Ball("Мяч", 0.5, 5, "green");
+        Item brick1 = new Brick("Кирпич", 2, 5, "brown");
+        Item magazine1 = new Magazine("Журнал", 1, 5, "grey");
+        Box box = new Box("Яндекс.Доставка", 0.5, 20);
+        Bag bag = new Bag("Delivery", 0.5, 10);
 
         assertDoesNotThrow(() ->{
             svg.fillHead();
 
-            /*brick.draw(svg, brick.getW() + 5,brick.getH() + 10 );
-            ball.draw(svg, ball.getW() + 5, ball.getH() + 50);
-            magazine.draw(svg, magazine.getW()+ 5, magazine.getH() + 100);*/
-
             box.add(magazine);
             box.add(brick);
             box.add(ball);
-            //box.add(bag);
 
+            bag.add(magazine1);
+            bag.add(brick1);
+            bag.add(ball1);
+
+            box.add(bag);
             box.draw(svg, 20, 100);
+
             svg.fillEndOfFile();
         });
     }
 
-    @Test
-    void simple () {
-    }
+
 
 }
