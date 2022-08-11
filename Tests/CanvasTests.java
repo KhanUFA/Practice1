703433394314e0cb3cc489eebb917ca298ad2e4b
Roman <lymon700@gmail.com>
2022-08-11 18:01:15 +0500

    Now test check only how container draw yourself

--- Tests/CanvasTests.java
+++ Tests/CanvasTests.java
@@ -17,37 +17,39 @@ public class CanvasTests {
     void simpleDraw () {
         String fileName = "simpleTest";
         SVGWriter svg = new SVGWriter(fileName);
-        Item ball = new Ball("Мяч", 0.5, 5, "green");
+        Item ball = new Ball("Мяч", 0.5, 2, "green");
         Item brick = new Brick("Кирпич", 2, 5, "brown");
         Item magazine = new Magazine("Журнал", 1, 5, "grey");
-        Item ball1 = new Ball("Мяч", 0.5, 5, "white");
+        Item ball1 = new Ball("Мяч", 0.5, 4, "white");
         Item brick1 = new Brick("Кирпич", 2, 5, "pink");
         Item magazine1 = new Magazine("Журнал", 1, 5, "black");
         Box box = new Box("Яндекс.Доставка", 0.5, 20);
-        Bag bag = new Bag("Delivery", 0.5, 10);
+        Bag bag = new Bag("Delivery", 0.5, 28);
         Shelf shelf = new Shelf("RAJTAN", 1,10,"#CD5C5C");
 
         assertDoesNotThrow(() ->{
             svg.fillHead();
+
+            box.add(ball);
             box.add(magazine);
             box.add(brick);
-            box.add(ball);
 
             bag.add(magazine1);
             bag.add(brick1);
             bag.add(ball1);
 
-            box.add(bag);
+            //box.add(bag);
             box.draw(svg, 20, 150);
-            bag.draw(svg, 400, 350);
+            bag.draw(svg, 200, 250);
 
             bag.remove(magazine1);
             bag.remove(brick1);
             bag.remove(ball1);
 
+            box.remove(ball);
             shelf.add(magazine1);
             shelf.add(brick1);
-            shelf.add(ball1);
+            shelf.add(ball);
 
             shelf.draw(svg, 20, 550);
 
